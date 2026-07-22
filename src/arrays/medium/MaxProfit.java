b60854962707c1527e8a2652f955b38870a4b570
pankaj yadav <paankajy@gmail.com>
2026-07-22 10:35:05 +0530

    arrays questions

---
+++ src/arrays/medium/MaxProfit.java
@@ -0,0 +1,27 @@
+package arrays.medium;
+
+public class MaxProfit {
+    public int method(int[] prices){
+        int profit = 0;
+        int buyPrice=prices[0];
+        int currentProfit=0;
+
+        for(int i=0;i<prices.length;i++){
+            if(prices[i] < buyPrice){
+                buyPrice = prices[i];
+            }
+            else{
+                currentProfit = prices[i] - buyPrice;
+                profit = Math.max(currentProfit, profit);
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        MaxProfit maxProfit = new MaxProfit();
+        int [] prices = {7,1,5,3,6,4};
+        //int [] prices = {7,6,4,3,1};
+        System.out.println(maxProfit.method(prices));
+    }
+}
