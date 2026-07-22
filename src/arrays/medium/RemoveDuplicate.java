bc9d58d38a33c9d97691002b2b9dee2f6dd130c7
pankaj yadav <paankajy@gmail.com>
2026-07-22 16:25:13 +0530

    Remove duplicate Array

---
+++ src/arrays/medium/RemoveDuplicate.java
@@ -0,0 +1,32 @@
+package arrays.medium;
+
+
+import java.util.*;
+
+public class RemoveDuplicate {
+
+    int method (int[] nums){
+        int j = 0;
+        for(int i=0; i<nums.length;i++){
+           if(nums[j] != nums[i]){
+               nums[++j] = nums[i];
+
+           }
+
+
+        }
+
+
+        return ++ j;
+    }
+    public static void main(String[] args) {
+        RemoveDuplicate removeDuplicate = new RemoveDuplicate();
+        //int [] nums = {1,1,2};
+        //int [] nums = {0,0,1,1,1,2,2,3,3,4};
+        //int [] nums = {0,0,3};
+        int [] nums = {-3,-1,0,0,0,3,3};
+
+
+        System.out.println(removeDuplicate.method(nums));
+    }
+}
